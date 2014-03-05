7ea847c30c5414a87d9d2fc19f8390a8652a904e
Wilberto <wilbertomorales777@gmail.com>
2014-03-05 12:52:53 -0600

    Removed old files and old libraries and replaced them with the latest libraries

--- snippets/flask101/importapp.py
+++
@@ -1,8 +0,0 @@
-from flask101 import app
-
-@app.route('/special')
-def special_feature():
-    return 'This only exists if the module ran is importapp.py'
-
-if __name__ == '__main__':
-    app.run()
--- snippets/flask101/name_and_main_example.py
+++
@@ -1,4 +0,0 @@
-from person import Person
-
-print "Importing Person shouldn't be doing anything. I only meant to import it for future use"
-print 'name_and_main_example.py __name__: %s' % __name__
